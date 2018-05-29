87c6c1b757b001b65d2b21a9fcf430e5a3cf209f
Matt Little <zone117x@gmail.com>
2018-05-29 10:37:24 -0700

    Example xunit test project

---
+++ ExampleUnitTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace DebugTeamCityTestReporting
+{
+    public class ExampleUnitTest
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true, "example passing test");
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.True(false, "example failing test");
+        }
+    }
+}
